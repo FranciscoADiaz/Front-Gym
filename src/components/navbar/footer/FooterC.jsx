3dcb52594537e21e13dae54b5b3d6cdafe3f4f58
valejusplay <vg9917755@gmail.com>
2025-06-28 21:42:09 -0300

    cre el navbar y footer

---
+++ src/components/navbar/footer/FooterC.jsx
@@ -0,0 +1,11 @@
+const FooterC = () => {
+  return (
+    <>
+      <footer className="bg-body-tertiary p-5  text-center">
+        <h3>Footer</h3>
+      </footer>
+    </>
+  );
+};
+
+export default FooterC;
