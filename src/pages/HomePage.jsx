003e2931ca2b14fb9de2b4d00a239eba2bb29e9f
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-25 01:51:51 -0300

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,32 @@
-
-import { useChangeTitle } from '../helpers/useChangeTitlePage'
-
-
-import home from '../assets/home.webp'
-import { Container } from 'react-bootstrap'
-import Wp from '../components/wp/BotonWhatsapp'
-import ejercicio from '../assets/ejercicio.jpeg'
+import { useChangeTitle } from '../helpers/useChangeTitlePage';
+import { Container } from 'react-bootstrap';
+import Wp from '../components/wp/BotonWhatsapp';
+import InfoGimnasio from '../components/home/InfoGimnasio';
+import Servicios from '../components/home/Servicios';
+import Productos from '../components/home/Productos';
+import Marcas from '../components/home/Marcas';
+import Comentarios from '../components/home/Comentarios';
+import Profesores from '../components/home/Profesores';
+import Planes from '../components/home/Planes';
 
 const HomePage = () => {
-  useChangeTitle("Inicio");
-  return (
-    <>
-      <Container idPage="inicio">
-        <img src={home} alt="" className="app-contenido-dos" />
-  <img src={ejercicio} alt="" className='app-contenido-dos' />
-        <Wp />
-      </Container>
-    </>
-  );
+  useChangeTitle("Inicio");
+
+  return (
+    <>
+      <Container idPage="inicio">
+       
+        <Wp />
+        <InfoGimnasio />
+        <Planes />
+        <Productos />
+        <Marcas />
+        <Comentarios />
+        <Profesores />
+        
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
