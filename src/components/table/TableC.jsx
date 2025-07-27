83e99d9779a945090597eb086db569d74a190f5b
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-26 21:24:02 -0300

    Update TableC.jsx

--- src/components/table/TableC.jsx
+++ src/components/table/TableC.jsx
@@ -122,17 +122,11 @@ const TableC = ({ array, idPage, funcionReseteador }) => {
                 </Button>
 
                 <Button
-                  className="mx-2"
-                  variant={
-                    element.estado === "habilitado" ? "secondary" : "success"
-                  }
-                  onClick={async () => {
-                    <Button
   className="mx-2"
   variant={element.estado === "habilitado" ? "secondary" : "success"}
   onClick={async () => {
     const accion =
-      element.estado === "habilitado" ? "deshabilitado" : "habilitado";
+      element.estado === "habilitado" ? "deshabilitar" : "habilitar";
     const mensaje = `¿${accion} usuario?`;
 
     const result = await Swal.fire({
@@ -158,37 +152,6 @@ const TableC = ({ array, idPage, funcionReseteador }) => {
 >
   {element.estado === "habilitado" ? "Deshabilitar" : "Habilitar"}
 </Button>
-                    const mensaje = `¿${accion} usuario?`;
-
-                    const result = await Swal.fire({
-                      title: mensaje,
-                      icon: "warning",
-                      showCancelButton: true,
-                      confirmButtonText: `Sí, ${accion}`,
-                      cancelButtonText: "Cancelar",
-                      confirmButtonColor: "#3085d6",
-                      cancelButtonColor: "#d33",
-                    });
-
-                    if (result.isConfirmed) {
-                      try {
-                        await habilitarDeshabilitarUsuario(element._id);
-                        funcionReseteador(); 
-                        Swal.fire(
-                          `Usuario ${accion} correctamente`,
-                          "",
-                          "success"
-                        );
-                      } catch {
-                        Swal.fire("Error al cambiar estado", "", "error");
-                      }
-                    }
-                  }}
-                >
-                  {element.estado === "habilitado"
-                    ? "Deshabilitar"
-                    : "Habilitar"}
-                </Button>
 
                 <Button variant="primary">Editar</Button>
               </td>
