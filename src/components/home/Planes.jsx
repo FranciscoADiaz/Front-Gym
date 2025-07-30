b135451cfd57dffd299a05f41dab8e9b4dd5f064
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-30 17:26:49 -0300

    Update Planes.jsx

--- src/components/home/Planes.jsx
+++ src/components/home/Planes.jsx
@@ -34,7 +34,7 @@ const Planes = () => {
   ];
 
   return (
-    <section className="py-5 my-5 bg-secondary text-white">
+    <section className="py-5 bg-secondary text-white">
       <div className="container">
         <h2 className="text-center mb-4">NUESTROS PLANES MENSUALES</h2>
         <Row className="g-4 justify-content-center">
