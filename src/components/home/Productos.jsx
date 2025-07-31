a650c01f0ffd21e4d22d329e50c9c44bf959c977
Francisco Ariel Diaz <123221707+FranciscoADiaz@users.noreply.github.com>
2025-07-31 16:13:39 -0300

    Update Productos.jsx

--- src/components/home/Productos.jsx
+++ src/components/home/Productos.jsx
@@ -50,7 +50,7 @@ const Productos = () => {
           {prod.titulo}
         </Card.Title>
 
-        <div className="bg-light text-dark py-2 my-0 text-center">
+        <div className="bg-light descripcion-card text-dark py-2 my-0 text-center">
           <Card.Text className="mx-2 descripcion-card">{prod.descripcion}</Card.Text>
         </div>
       </Card.Body>
